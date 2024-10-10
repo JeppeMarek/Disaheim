0481dcaa58e4f354b8c9de42ab05bb3ecfdd5d36
JeppeMarek <Jeppe_marek@hotmail.com>
2024-10-10 09:47:11 +0200

    Utility class created and UnitTest2 6/6

--- Disaheim/Program.cs
+++ Disaheim/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-        
+            Utility utility = new Utility();
+            Book bookOne = new Book("ID", "Title", 20);
+            Amulet amuletOne = new Amulet("ID", Level.high,"Design");
+            utility.GetValueOfBook(bookOne);
+            utility.GetValueOfAmulet(amuletOne);
+
         }
     }
 }
---
+++ Disaheim/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+            double bookValue = book.Price;
+            Console.WriteLine(bookValue);
+            return bookValue;
+        }
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            double amuletValue = 0;
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    amuletValue = 12.5;
+                    break;
+                case Level.medium:
+                    amuletValue = 20.0;
+                    break;
+                case Level.high:
+                    amuletValue = 27.5;
+                    break;
+            }
+            Console.WriteLine(amuletValue);
+            return amuletValue;
+        }
+    }
+}
